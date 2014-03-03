be8d9bc6f9604c27dbb5fdf291309020458c90fd
AmericanUmlaut <FamilieStuermer@MinChul>
2014-03-03 16:23:35 +0100

    Use $settings['bburl'] instead of hard-coding the url

--- inc/plugins/browserid.php
+++ inc/plugins/browserid.php
@@ -43,8 +43,9 @@ if (isset($_GET["assertion"])) {
  * */
 function Browserid_button($page)
 {
+    $forumUrlBase = $settings['bburl'];
     $page = str_replace(
-        "<a href=\"http://starbowmod.com/forum/member.php?action=login\" onclick=\"MyBB.quickLogin(); return false;\">Login</a> &mdash; <a href=\"http://starbowmod.com/forum/member.php?action=register\">Register</a>)</span>".PHP_EOL,
+        "<a href=\"$forumUrlBase/member.php?action=login\" onclick=\"MyBB.quickLogin(); return false;\">Login</a> &mdash; <a href=\"$forumUrlBase/member.php?action=register\">Register</a>)</span>".PHP_EOL,
         "<a id='browserid' href='member.php?action=login'>Login</a>)</span>".PHP_EOL.
         "<!-- end: header_welcomeblock_guest -->",
         $page
